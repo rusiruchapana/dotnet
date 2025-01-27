cf8d5abd75149215485f6e23d24b53171746612d
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2025-01-27 14:09:16 +0530

    Update Region.cs

--- dotnet_learn/Models/Domain/Region.cs
+++ dotnet_learn/Models/Domain/Region.cs
@@ -2,5 +2,8 @@ namespace dotnet_learn.Models.Domain;
 
 public class Region
 {
-    
+    public Guid Id { get; set; }
+    public string Code { get; set; }
+    public string Name { get; set; }
+    public string? RegionImageUrl { get; set; }
 }
