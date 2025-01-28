3033f6a000c60ddb9c64c142207f7ad1f9816c1a
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2025-01-29 00:27:14 +0530

    Update RegionController.cs

--- dotnet_learn/Controllers/RegionController.cs
+++ dotnet_learn/Controllers/RegionController.cs
@@ -1,5 +1,7 @@
 using dotnet_learn.Data;
 using dotnet_learn.Models.Domain;
+using dotnet_learn.Models.DTO;
+using dotnet_learn.Models.DTO.Request;
 using Microsoft.AspNetCore.Mvc;
 
 namespace dotnet_learn.Controllers;
@@ -8,17 +10,62 @@ namespace dotnet_learn.Controllers;
 [ApiController]
 public class RegionController: ControllerBase
 {
+    //DI
     private readonly DotNetLearnDbContext _context;
     public RegionController(DotNetLearnDbContext context)
     {
         _context = context;
     }
 
-
+    
+    //Get all regions.
     [HttpGet]
     public IActionResult GetAll()
     {
         List<Region> regions = _context.Regions.ToList();
-        return Ok(regions);
+        List<RegionDTO> regionDtos = new List<RegionDTO>();
+        
+        foreach (var region in regions)
+        {
+            RegionDTO regionDto = new RegionDTO
+            {
+                Id = region.Id,
+                Code = region.Code,
+                Name = region.Name,
+                RegionImageUrl = region.RegionImageUrl,
+            };
+            regionDtos.Add(regionDto);
+        }
+        return Ok(regionDtos);
     }
+
+    
+    
+    
+    //Create region.
+    [HttpPost]
+    public IActionResult AddRegion(AddRegionRequestDTO addRegionRequestDto)
+    {
+        var region = new Region
+        {
+            Id = Guid.NewGuid(),
+            Code = addRegionRequestDto.Code,
+            Name = addRegionRequestDto.Name,
+            RegionImageUrl = addRegionRequestDto.RegionImageUrl,
+        };
+        
+        _context.Regions.Add(region);
+        _context.SaveChanges();
+
+        RegionDTO regionDto = new RegionDTO
+        {
+            Id = region.Id,
+            Code = region.Code,
+            Name = region.Name,
+            RegionImageUrl = region.RegionImageUrl,
+        };
+        
+        return Ok(regionDto);
+    }
+
 }
