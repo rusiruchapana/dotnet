2dc816db82bcc227319176d0a830bc204bb87658
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2025-02-01 16:34:05 +0530

    Update Program.cs

--- dotnet_learn/Program.cs
+++ dotnet_learn/Program.cs
@@ -1,4 +1,5 @@
 using dotnet_learn.Data;
+using dotnet_learn.Repositories;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,6 +9,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<DotNetLearnDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<IRegionRepository , RegionRepositoryImpl>();
 builder.Services.AddControllers();
 
 
